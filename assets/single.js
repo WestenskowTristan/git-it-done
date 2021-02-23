4183f7bef2a1e42adda26082d336de6eb25c35ec
tristan <westenskowtristan122@gmail.com>
2021-02-23 16:43:24 -0700

    seeing repo issues on new page and solving for no issues on repos

---
+++ assets/single.js
@@ -0,0 +1,50 @@
+let issueContainerEl = document.querySelector("#issues-container");
+
+let getRepoIssues = function (repo) {
+  let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayIssues(data);
+      });
+    } else {
+      alert("There was a problem with your request!");
+    }
+  });
+};
+
+let displayIssues = function (issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+  for (let i = 0; i < issues.length; i++) {
+    //create a link element to take users to issue on github
+    let issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+    // create span to hold issue title
+    let titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    //append to container
+    issueEl.appendChild(titleEl);
+
+    //create a type element
+    let typeEl = document.createElement("span");
+
+    //check if issue is an actual issue or a pull requset
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+    issueContainerEl.appendChild(issueEl);
+
+    issueEl.appendChild(typeEl);
+  }
+};
+
+getRepoIssues("facebook/react");
