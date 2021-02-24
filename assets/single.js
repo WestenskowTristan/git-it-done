28c397dbbdc05caad10bbd4c92ca1cfdec15aef8
tristan <westenskowtristan122@gmail.com>
2021-02-23 18:00:09 -0700

    adding more functionality to page

--- assets/single.js
+++ assets/single.js
@@ -1,19 +1,42 @@
+let repoNameEl = document.querySelector("#repo-name");
 let issueContainerEl = document.querySelector("#issues-container");
 let limitWarningEl = document.querySelector("#limit-warning");
 
+let getRepoName = function () {
+  // grab repo name from url query string
+  let queryString = document.location.search;
+  let repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
 let getRepoIssues = function (repo) {
+  // format the github api url
   let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+  // make a get request to url
   fetch(apiUrl).then(function (response) {
+    // request was successful
     if (response.ok) {
       response.json().then(function (data) {
+        displayIssues(data);
+
+        // check if api has paginated issues
         if (response.headers.get("Link")) {
           displayWarning(repo);
         }
-        displayIssues(data);
       });
     } else {
-      alert("There was a problem with your request!");
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -23,44 +46,52 @@ let displayIssues = function (issues) {
     issueContainerEl.textContent = "This repo has no open issues!";
     return;
   }
+
+  // loop over given issues
   for (let i = 0; i < issues.length; i++) {
-    //create a link element to take users to issue on github
+    // create a link element to take users to the issue on github
     let issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
+
     // create span to hold issue title
     let titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
 
-    //append to container
+    // append to container
     issueEl.appendChild(titleEl);
 
-    //create a type element
+    // create a type element
     let typeEl = document.createElement("span");
 
-    //check if issue is an actual issue or a pull requset
+    // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
     } else {
       typeEl.textContent = "(Issue)";
     }
-    issueContainerEl.appendChild(issueEl);
 
+    // append to container
     issueEl.appendChild(typeEl);
+
+    // append to the dom
+    issueContainerEl.appendChild(issueEl);
   }
 };
 
 let displayWarning = function (repo) {
-  //add text to warning container
-  limitWarningEl.textContent = "To see more than 30 issues, visit";
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
+  // create link element
   let linkEl = document.createElement("a");
-  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.textContent = "GitHub.com";
   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
   linkEl.setAttribute("target", "_blank");
 
+  // append to warning container
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("microsoft/vscode");
+getRepoName();
