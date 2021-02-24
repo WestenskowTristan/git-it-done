192831ef4281d9fa6519cabb4994cc5e2d96a33b
tristan <westenskowtristan122@gmail.com>
2021-02-23 17:20:23 -0700

    adding link and message if repo has more than 30 issues

--- assets/single.js
+++ assets/single.js
@@ -1,4 +1,5 @@
 let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
 
 let getRepoIssues = function (repo) {
   let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -6,6 +7,9 @@ let getRepoIssues = function (repo) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
         displayIssues(data);
       });
     } else {
@@ -47,4 +51,16 @@ let displayIssues = function (issues) {
   }
 };
 
-getRepoIssues("facebook/react");
+let displayWarning = function (repo) {
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit";
+
+  let linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("microsoft/vscode");
